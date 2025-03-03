96a7e4a49215a42fa396dde7e148e2dd4db332f4
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-03-03 09:07:15 +0800

    feat: add data-attributes for the buttons

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ let a,b,operator;
 function add(a,b){
     return a + b;
 }
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+};
+
+function divide(a,b){
+    return a / b;
+}
+
+let x = document.querySelector(".digits");
+x.addEventListener('click', test);
+
+function test(e){
+
+    console.log(e.target.dataset.digit);
+}
